205679b23d513eb5e326e537526ff0e9b30388b2
kong lei <1063299919@qq.com>
2024-07-16 21:00:31 +0800

    test

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -104,7 +104,7 @@ int main(void)
   MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
-  printf("Smart Device Starthhh.\r\n");
+  // printf("Smart Device Starthhh.\r\n");
   // printf("Smart Device Starthhh.\r\n");
 
   SmartDeviceInit();
